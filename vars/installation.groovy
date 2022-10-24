8a3a2806e1058a77cd75a71d2f11230c6416200e
Leonid Lazarenko <leonid.lazarenko@netcracker.com>
2022-10-24 16:12:20 +1000

    install

--- vars/installation.groovy
+++ vars/installation.groovy
@@ -1,5 +1,5 @@
-def call(liferay_password, liferay_login)
+def call(liferay_password, liferay_login, inst)
 {
      git 'https://github.com/dot-lenyas/ansible-liferay'
-     ansiblePlaybook inventory: 'hosts', playbook: 'setup.yaml',  extras: "-e \"liferay_user=${liferay_login} admin_password=${liferay_password}\""  
+     ansiblePlaybook inventory: 'hosts', playbook: 'setup.yaml',  extras: "-e \"liferay_user=${liferay_login} admin_password=${liferay_password} install=${inst}\""  
 }
